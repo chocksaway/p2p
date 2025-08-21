a2a136aad8f0dc99a5a4429896e25a4ff82e7d16
Miles Davenport <milesd@chocksaway.com>
2025-08-21 21:02:58 +0100

    Simplify router.

--- src/main/java/com/chocksaway/p2p/route/Router.java
+++ src/main/java/com/chocksaway/p2p/route/Router.java
@@ -1,6 +1,7 @@
 package com.chocksaway.p2p.route;
 
 import com.chocksaway.p2p.Link;
+import com.chocksaway.p2p.message.SimpleMessage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,42 +9,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Router  {
+    private final String name;
     private final List<Link> links = new ArrayList<>();
     private static final Logger logger = LogManager.getLogger(Router.class);
+
+    public Router(String name) {
+        this.name = name;
+    }
 
     public void addLink(Link link) {
         if (link.from() == null || link.to() == null) {
             throw new NullPointerException("Source or destination is null");
-        } else if (!linkExists(link)) {
-            links.add(link);
-            send(link);
         }
+        links.add(link);
     }
 
-    public void send(Link link) {
-        link.sendMessage(link);
-    }
-
-    public boolean send(Link link, String message) {
-        if (linkExists(link)) {
+    public boolean send(SimpleMessage message) {
+        for (Link link : links) {
             link.sendMessage(message);
-//            if (true) { //todo: add a config option to control this
-//                links.stream()
-//                        .filter(each -> each != link)
-//                        .forEach(each -> each.sendMessage(message));
-//            }
-            return true;
         }
         return false;
-    }
-
-    public boolean linkExists(Link link) {
-        return links.stream()
-                .anyMatch(
-                        each ->
-                                each.from().getName().equals(link.from().getName()) &&
-                                    each.to().getName().equals(link.to().getName())
-                );
     }
 
     public int getLinks() {
